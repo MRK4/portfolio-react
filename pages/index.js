56c31a8e91e42812c06bff1db2a7ae85ab71ff7c
Clément Poudrée <markatio77@gmail.com>
2023-03-12 12:35:17 +0100

    Git init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Clément Poudrée</title>
+        <meta name="description" content="Développeur web front-end Rennais" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
